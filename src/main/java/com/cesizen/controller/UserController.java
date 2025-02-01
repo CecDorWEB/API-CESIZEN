26d2a53b23ab08f451bf3711e6f58515cce17c9a
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2025-02-01 12:54:53 +0100

    modification of user response during log in

--- src/main/java/com/cesizen/controller/UserController.java
+++ src/main/java/com/cesizen/controller/UserController.java
@@ -1,70 +1,77 @@
 package com.cesizen.controller;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cesizen.DTO.UserDTO;
 import com.cesizen.model.User;
+import com.cesizen.repository.UserRepository;
 import com.cesizen.services.UserServices;
-import com.cesizen.repository.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
 public class UserController {
-	
-	
+
 	@Autowired
 	private UserServices userServices;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@GetMapping
-    public List<UserDTO> getAllUsers() {
-        return userServices.getAllUsers()
-                .stream()
-                .map(userServices::toDTO) // Transformation User en UserDTO
-                .collect(Collectors.toList());
-    }
-	
+	public List<UserDTO> getAllUsers() {
+		return userServices.getAllUsers().stream().map(userServices::toDTO) // Transformation User en UserDTO
+				.collect(Collectors.toList());
+	}
+
 	@PostMapping("/login")
-	public ResponseEntity<Object> userLogin(@RequestBody Map<String, String> logInfos){
+	public ResponseEntity<Object> userLogin(@RequestBody Map<String, String> logInfos) {
 		String email = logInfos.get("email");
-	    String password = logInfos.get("password");
-		    
+		String password = logInfos.get("password");
+
 		Optional<User> optionalUser = userRepository.findByEmailAndPassword(email, password);
-		
-		if(optionalUser.isEmpty()) {
+
+		if (optionalUser.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Identifiant ou mot de passe invalides.");
 		}
-		
-			User user = optionalUser.get();
-			
-			if (user.getStatut() == true) {
-				
-				 Map<String, Object> userResponse = new HashMap<>();
-			        userResponse.put("id", user.getId()); // ID de l'utilisateur
-			        userResponse.put("firstname", user.getFirstname());
-			        userResponse.put("lastname", user.getLastname());
-			        userResponse.put("email", user.getEmail());     // Email de l'utilisateur
-			        userResponse.put("role", user.getRole().getId());       // Rôle de l'utilisateur
 
-			        return ResponseEntity.ok(userResponse);
-				} else {
-					return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Connexion refusée, compte suspendu !");
-					} 
-			}
-	
-	 @PostMapping
-	    public UserDTO createUser(@RequestBody UserDTO userDTO) {
-		 User user = userServices.toEntity(userDTO);
-	        User savedUser = userServices.saveUser(user);
+		User user = optionalUser.get();
 
-	        return userServices.toDTO(savedUser);
-	    }
+		if (user.getStatut() == true) {
+
+			Map<String, Object> userResponse = new HashMap<>();
+			userResponse.put("id", user.getId()); // ID de l'utilisateur
+			userResponse.put("firstname", user.getFirstname());
+			userResponse.put("lastname", user.getLastname());
+			userResponse.put("email", user.getEmail()); // Email de l'utilisateur
+			userResponse.put("type", user.getRole().getName());
+			userResponse.put("role", user.getRole().getId()); // Rôle de l'utilisateur
+
+			return ResponseEntity.ok(userResponse);
+		} else {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Connexion refusée, compte suspendu !");
+		}
+	}
+
+	@PostMapping
+	public UserDTO createUser(@RequestBody UserDTO userDTO) {
+		User user = userServices.toEntity(userDTO);
+		User savedUser = userServices.saveUser(user);
+
+		return userServices.toDTO(savedUser);
+	}
 }
