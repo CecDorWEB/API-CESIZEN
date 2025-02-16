d175bcd5eea8118d69eebdb237aa9b3b953f1c09
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2025-02-16 12:57:37 +0100

    Add unique road postmapping called createRessource to create Test or Article

--- src/main/java/com/cesizen/controller/RessourceController.java
+++ src/main/java/com/cesizen/controller/RessourceController.java
@@ -6,10 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cesizen.DTO.RessourceDTO;
+import com.cesizen.model.Ressource;
 import com.cesizen.repository.RessourceRepository;
 import com.cesizen.services.RessourceServices;
 
@@ -29,14 +32,14 @@ public class RessourceController {
 		return ressourceServices.getAllRessources().stream().map(ressourceServices::toDTO).collect(Collectors.toList());
 	}
 
-	/*
-	 * // Créer une nouvelle ressource
-	 * 
-	 * @PostMapping public RessourceDTO createRessource(@RequestBody RessourceDTO
-	 * ressourceDTO) { Ressource ressource =
-	 * ressourceServices.toEntity(ressourceDTO); Ressource savedRessource =
-	 * ressourceServices.saveRessource(ressource);
-	 * 
-	 * return ressourceServices.toDTO(savedRessource); }
-	 */
+	// Créer une nouvelle ressource
+
+	@PostMapping
+	public RessourceDTO createRessource(@RequestBody RessourceDTO ressourceDTO) {
+		Ressource ressource = ressourceServices.toEntity(ressourceDTO);
+		Ressource savedRessource = ressourceServices.saveRessource(ressource);
+
+		return ressourceServices.toDTO(savedRessource);
+	}
+
 }
--- src/main/java/com/cesizen/services/RessourceServices.java
+++ src/main/java/com/cesizen/services/RessourceServices.java
@@ -1,5 +1,6 @@
 package com.cesizen.services;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cesizen.DTO.RessourceDTO;
+import com.cesizen.model.Article;
 import com.cesizen.model.Ressource;
+import com.cesizen.model.Test;
+import com.cesizen.model.TypeRessource;
+import com.cesizen.model.User;
 import com.cesizen.repository.RessourceRepository;
 import com.cesizen.repository.TypeRessourceRepository;
 import com.cesizen.repository.UserRepository;
@@ -34,47 +39,59 @@ public class RessourceServices {
 	LocalDate today = LocalDate.now();
 
 	public RessourceDTO toDTO(Ressource ressource) {
-		/**
-		 * 
-		 * String ressourceType;
-		 * 
-		 * if (ressource instanceof Article) { ressourceType = "article"; } else if
-		 * (ressource instanceof Test) { ressourceType = "test"; } else { throw new
-		 * IllegalArgumentException("Unknown ressource type"); }
-		 * 
-		 */
+		Long ressourceType;
+
+		if (ressource instanceof Article) {
+			ressourceType = 1L;
+		} else if (ressource instanceof Test) {
+			ressourceType = 2L;
+		} else {
+			throw new IllegalArgumentException("Unknown ressource type");
+		}
+
 		return new RessourceDTO(ressource.getId(), ressource.getTitle(), ressource.getHeaderImage(),
 				ressource.getHeaderIntroduction(), ressource.getPublicationDate(), ressource.getUpdateDate(),
-				ressource.isStatut(), ressource.getTypeRessource().getId(), ressource.getUser().getId());
+				ressource.isStatut(), ressourceType, ressource.getUser().getId());
 
 	}
 
-	/*
-	 * // Transformer le modèle DTO (incomplet) en Entité Ressource correspondant
-	 * aux // champs de la BDD public Ressource toEntity(RessourceDTO ressourceDTO)
-	 * {
-	 * 
-	 * // ajouter les if pour les classes abstraites Ressource ressource = new
-	 * Ressource(); ressource.setId(ressourceDTO.id());
-	 * ressource.setTitle(ressourceDTO.title());
-	 * ressource.setHeaderImage(ressourceDTO.headerImage());
-	 * ressource.setHeaderIntroduction(ressourceDTO.headerIntroduction());
-	 * ressource.setPublicationDate(Date.valueOf(LocalDate.now()));
-	 * ressource.setUpdateDate(null); ressource.setStatut(true);
-	 * 
-	 * // Récupérer le type de la ressource TypeRessource typeRessource =
-	 * typeRessourceRepository.findById(ressourceDTO.type_id()) .orElseThrow(() ->
-	 * new RuntimeException("Type with ID " + ressourceDTO.type_id() +
-	 * "not found")); ressource.setTypeRessource(typeRessource);
-	 * 
-	 * // Récupérer l'id du user à l'origine de la création de la ressource User
-	 * user = userRepository.findById(ressourceDTO.user_id()) .orElseThrow(() -> new
-	 * RuntimeException("Type with ID " + ressourceDTO.user_id() + "not found"));
-	 * ressource.setUser(user);
-	 * 
-	 * return ressource;
-	 * 
-	 * }
-	 */
+	// Transformer le modèle DTO (incomplet) en Entité Ressource correspondant aux
+	// //
+	// champs de la BDD
+	public Ressource toEntity(RessourceDTO ressourceDTO)
+
+	{
+		// ajouter les if pour les classes abstraites
+		Ressource ressource;
+
+		if (ressourceDTO.type_id().equals(1L)) {
+			ressource = new Article();
+		} else if (ressourceDTO.type_id().equals(2L)) {
+			ressource = new Test();
+		} else {
+			throw new IllegalArgumentException("Invalid ressource type ID: " + ressourceDTO.type_id());
+		}
+
+		ressource.setId(ressourceDTO.id());
+		ressource.setTitle(ressourceDTO.title());
+		ressource.setHeaderImage(ressourceDTO.headerImage());
+		ressource.setHeaderIntroduction(ressourceDTO.headerIntroduction());
+		ressource.setPublicationDate(Date.valueOf(LocalDate.now()));
+		ressource.setUpdateDate(null);
+		ressource.setStatut(true);
+
+		// Récupérer le type de la ressource
+		TypeRessource typeRessource = typeRessourceRepository.findById(ressourceDTO.type_id())
+				.orElseThrow(() -> new RuntimeException("Type with ID " + ressourceDTO.type_id() + "not found"));
+		ressource.setTypeRessource(typeRessource);
+
+		// Récupérer l'id du user à l'origine de la création de la ressource
+		User user = userRepository.findById(ressourceDTO.user_id())
+				.orElseThrow(() -> new RuntimeException("Type with ID " + ressourceDTO.user_id() + "not found"));
+		ressource.setUser(user);
+
+		return ressource;
+
+	}
 
 }
